bb6e14577934916c21a4c4482dba0b81ecba0f4e
wallace3 <edhernandezw@gmail.com>
2023-04-25 20:41:58 -0600

    diseño a vista de login

--- vue-light-bootstrap-dashboard-master/src/pages/Login.vue
+++ vue-light-bootstrap-dashboard-master/src/pages/Login.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="container">
+  <div class="container login-container">
     <div class="row">
-        <div class="col-md-6 mt-5 mx-auto">
+        <div class="col-md-6 mt-5 mx-auto login-form-1">
             <form v-on:submit.prevent="login">
-              <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+              <h1 class="h3 mb-3 font-weight-normal">Inicio de Sesión</h1>
 
               <div class="form-group">
-                <label for="email"> Email Address</label>
-                <input type="email" v-model="credentials.email" class="form-control" name="email" placeholder="Email Address">
+                <label for="email"> Correo</label>
+                <input type="email" v-model="credentials.email" class="form-control" name="email" placeholder="Corre">
               </div>
 
               <div class="form-group">
-                <label for="password"> Password</label>
-                <input type="password" v-model="credentials.password" class="form-control" name="password" placeholder="Password">
+                <label for="password"> Contraseña </label>
+                <input type="password" v-model="credentials.password" class="form-control" name="password" placeholder="Contraseña">
               </div>
 
-              <button type="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
+              <button type="submit" class="btn btn-lg btn-primary btn-block">Iniciar Sesión</button>
             </form>
       </div>
     </div>
@@ -46,3 +46,59 @@ export default{
   }
 }
 </script>
+<style>
+
+.login-container{
+    margin-top: 5%;
+    margin-bottom: 5%;
+}
+.login-form-1{
+    padding: 5%;
+    box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 9px 26px 0 rgba(0, 0, 0, 0.19);
+}
+.login-form-1 h3{
+    text-align: center;
+    color: #333;
+}
+.login-form-2{
+    padding: 5%;
+    background: #0062cc;
+    box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 9px 26px 0 rgba(0, 0, 0, 0.19);
+}
+.login-form-2 h3{
+    text-align: center;
+    color: #fff;
+}
+.login-container form{
+    padding: 10%;
+}
+.btnSubmit
+{
+    width: 50%;
+    border-radius: 1rem;
+    padding: 1.5%;
+    border: none;
+    cursor: pointer;
+}
+.login-form-1 .btnSubmit{
+    font-weight: 600;
+    color: #fff;
+    background-color: #0062cc;
+}
+.login-form-2 .btnSubmit{
+    font-weight: 600;
+    color: #0062cc;
+    background-color: #fff;
+}
+.login-form-2 .ForgetPwd{
+    color: #fff;
+    font-weight: 600;
+    text-decoration: none;
+}
+.login-form-1 .ForgetPwd{
+    color: #0062cc;
+    font-weight: 600;
+    text-decoration: none;
+}
+
+</style>
